b0809635813633697539167fa8e32559d7d0e41a
Trey Rader <treyrdr09@gmail.com>
2016-08-11 13:35:51 -0500

    5 sections added with questions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$('#first-page-btn').click(function() {
 		$('#first-page').fadeOut(1000);
 		$('#second-page').fadeIn(1000);
@@ -13,4 +13,29 @@ $(document).ready(function() {
 		$('#third-page').fadeOut(1000);
 		$('#first-page').fadeIn(1000);
 	})
-})
+})*/
+
+
+	//Gabe's code below
+
+$(document).ready(function() {
+	var questions = ['q-1', 'q-2', 'q-3', 'q-4', 'q-5'];
+	for (var i=0; i<questions.length; i++) {
+		//console.log(pages[i]);
+		//console.log(pages);
+		var question = $('#' + questions[i]);
+		
+		question.find('.btn').click(function() {
+			var question = $(this).closest('section');
+			var nextQuestion = question.next();
+
+			$(question).fadeOut(1000);
+			$(nextQuestion).fadeIn(1000);
+		})
+	}
+		
+
+
+});
+	
+	
