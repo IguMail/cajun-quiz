c7b8f40fd51beafa4e63066d6903fe1d9f7e9e0a
Trey Rader <treyrdr09@gmail.com>
2016-08-10 23:47:43 -0500

    experimenting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$('#first-page-btn').click(function() {
 		$('#first-page').fadeOut(1000);
 		$('#second-page').fadeIn(1000);
@@ -13,4 +13,29 @@ $(document).ready(function() {
 		$('#third-page').fadeOut(1000);
 		$('#first-page').fadeIn(1000);
 	})
-})
+})*/
+
+
+	//Gabe's code below
+
+$(document).ready(function() {
+	var pages = ['first-page', 'second-page', 'third-page'];
+	for (var i=0; i<pages.length; i++) {
+		//console.log(pages[i]);
+		//console.log(pages);
+		var page = $('#' + pages[i]);
+		
+		$(page).find('.btn').click(function() {
+			var page = $(this).closest('section');
+			var nextPage = $(page).next();
+
+			$(page).fadeOut(1000);
+			$(nextPage).fadeIn(1000);
+		})
+	}
+		//console.log(page);
+
+
+});
+	//var firstPage = $('#' + pages[0]);
+	
